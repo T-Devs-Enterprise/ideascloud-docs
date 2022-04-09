915a2c3d59b04b454add334489df3b01c75bdca9
Frank Cary <fcaryv@gmail.com>
2022-04-09 03:19:36 -0500

    first commit

---
+++ components/templates/Guide.vue
@@ -0,0 +1,37 @@
+<template>
+  <AppPage class="d-container">
+    <PageContent :page="page">
+      <template #mobile-toc>
+        <PageMobileToc
+          :title="page.body.toc.title"
+          :toc="page.body.toc.links"
+        />
+      </template>
+    </PageContent>
+
+    <EditOnGithub v-if="repoUrl" :page="page" />
+
+    <template #toc>
+      <PageToc
+        v-if="!page.body.hideToc"
+        :title="page.body.toc.title"
+        :toc="page.body.toc.links"
+      />
+    </template>
+  </AppPage>
+</template>
+
+<script lang="ts">
+//import { defineComponent, onMounted } from "@nuxtjs/composition-api";
+//import { useGitHub, scrollToHeading } from "@docus/theme/runtime";
+
+export default {
+  props: {
+    page: {
+      type: Object,
+      required: true,
+    },
+  },
+  
+};
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,54 @@
+import theme from "@nuxt/content-theme-docs";
+
+export default theme({
+  docs: {
+    primaryColor: "#3e76df",
+  },
+  content: {
+    //apiPrefix: "testVue#/",
+  },
+  contents: {
+    markdown: {
+      remarkPlugins: ["~~/plugins/contributors.js"],
+    },
+  },
+  head: {
+    link: [{ rel: "icon", type: "image/x-icon", href: "/icon.png" }],
+  },
+  i18n: {
+    locales: () => [
+      {
+        code: "es",
+        iso: "es-ES",
+        file: "es-ES.js",
+        name: "Español",
+      },
+      {
+        code: "en",
+        iso: "en-US",
+        file: "en-US.js",
+        name: "English",
+      },
+    ],
+    defaultLocale: "es",
+  },
+
+  //srcDir: ".",
+  //generate: {
+  //  subFolders: false,
+  //},
+  //ssr: false,
+
+  //target: "static",
+  //router: {
+  //  trailingSlash: true,
+  //  base: "/testVue/",
+  //  mode: "hash",
+  //},
+  build: {
+    //publicPath: "testVue#/",
+  },
+  //static: {
+  //  prefix: true,
+  //},
+});
